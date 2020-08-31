5a6a47e1e7b00c8c4343d42bd16885e174b34a6b
sifat007 <sifat27.ht@gmail.com>
2020-08-31 19:46:44 +0600

    search data from recycler view make filtter functionality

--- app/src/main/java/com/example/roomadvancewithserverclient/ViewActivity.java
+++ app/src/main/java/com/example/roomadvancewithserverclient/ViewActivity.java
@@ -8,52 +8,83 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class ViewActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     RecyclerView.Adapter adapter;
     RecyclerView.LayoutManager layoutManager;
-    ArrayList<Student> studentArrayList;
+    ArrayList<Student> studentArrayList, studentArrayList_Search;
+    EditText searchText;
+    CustomAdapter customAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
 
-        recyclerView=findViewById(R.id.my_recView);
+        searchText = findViewById(R.id.search_id);
+        recyclerView = findViewById(R.id.my_recView);
         recyclerView.setHasFixedSize(true);
 
-        layoutManager=new LinearLayoutManager(this);
+        layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
-       new  LoadDataTask().execute();
+
+        searchText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                    String text=charSequence.toString();
+                Filter(text);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+
+        new LoadDataTask().execute();
+
+
+
 
     }
 
-    class LoadDataTask extends AsyncTask<Void,Void,Void>
-    {
+    class LoadDataTask extends AsyncTask<Void, Void, Void> {
         StudentRepository studentRepository;
         List<Student> studentList;
-
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            studentRepository=new StudentRepository(getApplicationContext());
-
+            studentRepository = new StudentRepository(getApplicationContext());
         }
 
         @Override
         protected Void doInBackground(Void... voids) {
-            studentList=studentRepository.getStudents();
-            studentArrayList=new ArrayList<>();
+            studentList = studentRepository.getStudents();
+            studentArrayList = new ArrayList<>();
+            studentArrayList_Search = new ArrayList<>();//for search
 
-            for (int i=0;i<studentList.size();i++)
-            {
+            for (int i = 0; i < studentList.size(); i++) {
                 studentArrayList.add(studentList.get(i));
+                studentArrayList_Search.add(studentList.get(i));//for search
             }
 
             return null;
@@ -63,9 +94,35 @@ public class ViewActivity extends AppCompatActivity {
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
 
-            CustomAdapter customAdapter=new CustomAdapter(studentArrayList,ViewActivity.this);
+            customAdapter = new CustomAdapter(studentArrayList, ViewActivity.this);
             recyclerView.setAdapter(customAdapter);
         }
     }
+    ////////==========================FILTER METHODS==========================================////////////////////////
+
+    public void Filter(String charText) {
+
+        charText = charText.toLowerCase(Locale.getDefault());
+        Log.d("filter", charText + "");
+
+        studentArrayList.clear();
+
+        if (charText.length() == 0) {
+            studentArrayList.addAll(studentArrayList_Search);
+            Log.d("load data", "all");
+        } else {
+            Log.d("load data", "filtter");
+            for (Student student : studentArrayList_Search) {
+                if (student.student_name.toLowerCase(Locale.getDefault()).contains(charText)||student.contanctno.toLowerCase(Locale.getDefault()).contains(charText)) {
+                    studentArrayList.add(student);
+                }
+            }
+
+        }
+        customAdapter.notifyDataSetChanged();
+    }
+
+
+    ////////==========================FILTER METHODS==========================================////////////////////////
 
 }
